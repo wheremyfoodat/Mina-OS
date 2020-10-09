233f9e58abff749988387ade9c23944560245e86
wheremyfoodat <44909372+wheremyfoodat@users.noreply.github.com>
2020-10-10 00:00:53 +0300

    Add files via upload

---
+++ src/core/boot.asm
@@ -0,0 +1,45 @@
+include "common/hardware.inc"
+include "syscalls.asm"
+
+; Addresses of important vectors:
+; exception vector (Every fault event, including SVCalls, reset, etc, goes through this vector)
+; depending on the event type, the exception vector jumps to one of these vectors:
+
+; supervisorCallHandler: 300h
+; bootSequence: 1000h
+
+section "exception vector" [0h]
+
+.handleException:
+
+    // Load FAULT into r14
+    mfrc r14
+    andi r14, r14, #F00h
+    lsr r14, r14, #8
+
+    cmpi/eq r14, FAULT_SVCALL // If FAULT == 14, call the SVCALL handler
+    ct supervisorCallHandler
+
+    cmpi/eq r14, FAULT_RESET
+    bt bootSequence // If FAULT == 15 (state of FAULT on boot), jump to the reset sequence
+
+    stop // If FAULT is none of these values, this means we've stumbled upon an exception I haven't implemented. If so, STOP.
+
+
+// POST code
+section "boot sequence" [1000h]
+
+bootSequence:
+    mov r15, RAM_START ; init the SP
+
+.greetUser:
+    mov r0, .helloMessage    
+    mov r1, MMIO_START
+    svcall printString
+
+// TODO
+.POST:
+    stop
+
+.helloMessage:
+    db "This is sir Michel Rodrique, speaking to you from the white house.\n", 0
---
+++ src/core/syscalls.asm
@@ -0,0 +1,45 @@
+include "common/hardware.inc"
+
+; Supervisor calls are laid in the ROM starting at 0x2000.
+; Each supervisor call is 128 (0x80) bytes long
+; So SVCALL #0 would be at 0x2000
+; SVCALL #1 would be at 0x2080, etc
+equ SVCALL_TABLE_START, 0x2000
+
+section "supervisor call handler" [300h]
+
+supervisorCallHandler:
+    push r14 ; Push r14, which contains the current FAULT event
+
+    mfrc r14
+    andi r14, r14, FFh ; Fetch the supervisor call comment from the control register
+    lsr r14, r14, 7 ; Multiply the comment by 0x80 to find the address of the fired SVCALL
+    addi r14, r14, SVCALL_TABLE_START
+    rcall r14, 0
+
+    pop r14 ; Restore r14
+    ret
+
+; params:
+; r0: start address of the string to print 
+SVCALL_printString:
+    push r0
+    push r1
+    push r2
+
+    mov r1, MMIO_START
+
+.printLoop:
+    ld r2, [r1, rCHAR]
+    cmpi/eq r2, #0 // If the char is a null terminator, stop printing
+    bt .SVCALL_printString_exit
+
+    st r2, [r1, rCHAR] // Print a character
+    addi r2, r2, #1
+    bra .printLoop
+
+.SVCALL_printString_exit:
+    pop r2
+    pop r1
+    pop r0
+    ret
